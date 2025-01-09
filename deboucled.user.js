6005f0ce4b503f5c7d6cc726ca6ae08a002bdf41
Victorien Plancke <victorien@plkproduction.com>
2025-01-09 16:54:06 +0100

    New version

--- deboucled.user.js
+++ deboucled.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.60.6
+// @version     2.60.8
 // @downloadURL https://github.com/vitoo/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/vitoo/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
