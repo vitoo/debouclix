82901e334bfb0c0f63aac4ac812e5631eea2f612
Victorien Plancke <victorien@plkproduction.com>
2026-01-03 14:51:19 +0100

    Bump version to 2.60.14 in meta and user scripts

--- deboucled.meta.js
+++ deboucled.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Déboucled
 // @namespace   deboucledjvcom
-// @version     2.60.13
+// @version     2.60.14
 // @downloadURL https://github.com/vitoo/deboucled/raw/master/deboucled.user.js
 // @updateURL   https://github.com/vitoo/deboucled/raw/master/deboucled.meta.js
 // @author      Rand0max
